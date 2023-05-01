8ce0843529c9f84eb232b0380e3f472d35640f77
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-02 01:41:08 +0430

    Installation done for: react, redux-logger,linters

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi, I am here</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
