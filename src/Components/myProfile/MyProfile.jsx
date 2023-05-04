711ca4b098f9c8615565b459333da65af3c47f90
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-05-04 16:31:52 +0500

    Add Missions in MyProfile.jsx

--- src/Components/myProfile/MyProfile.jsx
+++ src/Components/myProfile/MyProfile.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import '../../styles/myProfile-reservedRockets.css';
+import '../../styles/myProfile.css';
 
 const MyProfile = () => {
   const stateData = useSelector((state) => (state.reservationListKey.reservationList));
-
   const reservedRockets = stateData.filter((rocket) => (rocket.reserved));
 
+  const { missionsArray } = useSelector((store) => (store.missionsReducer));
+  const myProfileArray = missionsArray?.filter((trueReserved) => trueReserved.reserved === true);
+
   return (
-    <section className="reserved-rockets">
+    <section className="reserved-missions-rockets">
+      <div className="missions-list-container">
+        <h2>My Missions</h2>
+        <ul className="missions-list">
+          {
+            myProfileArray?.map((item) => (
+              <li className="missions-item" key={item.mission_id} id={item.mission_id}>
+                <h2>{item.mission_name}</h2>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
       <div className="rocket-list-container">
         <h2>My Rockets</h2>
         <ul className="rocket-list">
