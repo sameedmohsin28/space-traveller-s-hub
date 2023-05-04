0ed030ac5260c2745bac326f6ce834cf62224a0d
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-05-04 19:41:24 +0430

    Header test and RocketFetch test added

---
+++ src/test/RocketsFetch.test.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import rocketApiMock from './Mocks/Rocket_API_MOCK';
+
+describe('Validation for fetching Rockets', () => {
+  test('Data should be fetched successfully', () => {
+    // Act
+    const data = rocketApiMock();
+    // Assert
+    expect(data).toEqual([{ id: 1, name: 'ok' }]);
+  });
+});
